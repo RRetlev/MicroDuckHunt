e95fe795228b8a09317230d4d5c9b2802f064ceb
Gergely Simonics <gergelysimonics@Gergelys-MacBook-Pro.local>
2019-10-01 14:19:34 +0200

    configured shooter on port:8091

--- shooter/src/main/java/com/codecool/shooter/ShooterApplication.java
+++ shooter/src/main/java/com/codecool/shooter/ShooterApplication.java
@@ -2,8 +2,12 @@ package com.codecool.shooter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableSwagger2
 public class ShooterApplication {
 
     public static void main(String[] args) {
