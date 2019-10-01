d99d8597cff6b6c7a0aa9e2b0bbecf515113dec5
Retlev <rretlev@gmail.com>
2019-10-01 12:58:42 +0200

    initial commit x

---
+++ shooter/src/main/java/com/codecool/shooter/ShooterApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.shooter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShooterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShooterApplication.class, args);
+    }
+
+}
